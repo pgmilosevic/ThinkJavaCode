74b9e8f69f92eae564f106715990cab0213499af
pgmilosevic <66802010+pgmilosevic@users.noreply.github.com>
2020-09-20 19:59:54 +0200

    Add files via upload

---
+++ ThinkJavaCode/ch02/Date.java
@@ -0,0 +1,15 @@
+public class Date
+{
+  public static void main(String [] args) 
+  {
+    String dan = "utorak";
+    int datum = 15;
+    String mesec = "septembar";
+    int godina = 2020;
+    System.out.println("Američki format je: ");
+    System.out.println( dan + ", " + mesec + " " + datum + ", " + godina );
+    System.out.println("Evropski format je: ");
+    System.out.println( dan + " " + datum + " " + mesec + " " + godina );
+  }
+}
+  
